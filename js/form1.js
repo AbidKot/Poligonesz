0387bf6df710eee93630e92eda306af79c47c41d
AbidKot <m-10684589@moe-dl.edu.my>
2025-03-29 09:14:47 +0800

    Update form1.js

--- js/form1.js
+++ js/form1.js
@@ -1,12 +1,9 @@
 function generateForm1Question() {
-    const variables = ["a", "b", "c", "d", "m", "n"];
-    const var1 = variables[Math.floor(Math.random() * variables.length)];
-    const var2 = variables[Math.floor(Math.random() * variables.length)];
     const num1 = Math.floor(Math.random() * 10) + 1;
     const num2 = Math.floor(Math.random() * 10) + 1;
-    const num3 = Math.floor(Math.random() * 5) + 1;
+    const question = `Simplify: ${num1}x + ${num2}x`;
+    const answer = ((num1 + num2) + "x").toString();
 
-    const question = `Simplify: ${num1}${var1} + ${num2}${var2} - ${num3}${var1}`;
-    const answer = `${num1 - num3}${var1} + ${num2}${var2}`;
-    return { question, answer };
+    document.getElementById("question").textContent = question;
+    sessionStorage.setItem("correctAnswer", answer);
 }
